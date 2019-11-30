f1cf4cbc42c61f668a4f653ad7cd5629d9b8c653
Avanish82 <avanish.as@gmail.com>
2019-11-30 22:08:37 +0530

    first commit

---
+++ shakajs/js/header.js
@@ -0,0 +1,7 @@
+window.addEventListener('load', function () {
+	var player = location.pathname.split('/').slice(-1)[0];
+	var header = document.getElementsByTagName("header")[0];
+
+	if (player == 'mpx.html' || player == 'index.html') {
+}
+ 
